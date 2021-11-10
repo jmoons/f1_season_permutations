6ed03fef96cc9be2b4e250ff0129f9a0d07903cf
John Moons <jmoons@realdigitalmedia.com>
2021-11-10 05:48:00 -0500

    First pass at loops to produce the permutations in the desired data model

---
+++ README.md
@@ -0,0 +1,1 @@
+Let's have some fun with Ruby working through all the F1 permutations remaining
---
+++ season_permutations.rb
@@ -0,0 +1,54 @@
+# Data Model
+# {
+#   1 => {
+#     "MaxPosition" => 1,
+#     "LewisPosition" => 1,
+#     "FastestLap" => "Max" | "Lewis" | "Neither",
+#     "MaxPoints" => 1,
+#     "LewisPoints" => 1,
+#   }
+# }
+
+results = {}
+result_count = 1
+
+MAX_NAME = "Max"
+LEWIS_NAME = "Lewis"
+POSTION_NAME = "Position"
+NEITHER_NAME = "Neither"
+POINTS_NAME = "Points"
+FASTEST_LAP_NAME = "FastestLap"
+
+(1..11).each do | max_position |
+  (1..11).each do | lewis_position |
+    next if ( max_position < 11 && lewis_position < 11 && max_position == lewis_position )
+    (1..3).each do | fastest_lap_winner |
+      case fastest_lap_winner
+      when 1
+        results[result_count] = { (MAX_NAME + POSTION_NAME) => max_position,
+                                  (LEWIS_NAME + POSTION_NAME) => lewis_position,
+                                  FASTEST_LAP_NAME => MAX_NAME,
+                                  (MAX_NAME + POINTS_NAME) => 1,
+                                  (LEWIS_NAME + POINTS_NAME) => 1
+                                }
+      when 2
+        results[result_count] = { (MAX_NAME + POSTION_NAME) => max_position,
+                                  (LEWIS_NAME + POSTION_NAME) => lewis_position,
+                                  FASTEST_LAP_NAME => LEWIS_NAME,
+                                  (MAX_NAME + POINTS_NAME) => 1,
+                                  (LEWIS_NAME + POINTS_NAME) => 1
+                                }
+      when 3
+        results[result_count] = { (MAX_NAME + POSTION_NAME) => max_position,
+                                  (LEWIS_NAME + POSTION_NAME) => lewis_position,
+                                  FASTEST_LAP_NAME => NEITHER_NAME,
+                                  (MAX_NAME + POINTS_NAME) => 1,
+                                  (LEWIS_NAME + POINTS_NAME) => 1
+                                }
+      end
+      result_count += 1
+    end
+  end
+end
+
+puts results.inspect
