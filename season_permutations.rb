94a22d6afb3ef1a2be02c5abc78faabf802810f4
John Moons <jmoons@realdigitalmedia.com>
2021-11-10 15:31:13 -0500

    First pass at working through both race permutations.

--- season_permutations.rb
+++ season_permutations.rb
@@ -25,15 +25,15 @@
 
 RACE_PERMUTATION_INITIALIZATION_MAX_POINTS_KEY = "max_initial_points"
 RACE_PERMUTATION_INITIALIZATION_LEWIS_POINTS_KEY = "lewis_initial_points"
+MAX_NAME = "Max"
+LEWIS_NAME = "Lewis"
+POSTION_NAME = "Position"
+NEITHER_NAME = "Neither"
+POINTS_NAME = "Points"
+FASTEST_LAP_NAME = "FastestLap"
 
 class RacePermutations
 
-  MAX_NAME = "Max"
-  LEWIS_NAME = "Lewis"
-  POSTION_NAME = "Position"
-  NEITHER_NAME = "Neither"
-  POINTS_NAME = "Points"
-  FASTEST_LAP_NAME = "FastestLap"
   POINTS_PER_POSITION = { 1 => 25, 2 => 18, 3 => 15, 4 => 12, 5 => 10, 6 => 8, 7 => 6, 8 => 4, 9 => 2, 10 => 1 }
   POINTS_FOR_FASTEST_LAP = 1
 
@@ -99,4 +99,29 @@ end
 
 race_one_permutations = RacePermutations.new( {RACE_PERMUTATION_INITIALIZATION_MAX_POINTS_KEY => 312.5, RACE_PERMUTATION_INITIALIZATION_LEWIS_POINTS_KEY => 293.5} ).execute_permutations
 
-puts race_one_permutations.inspect
+race_two_results = {}
+race_two_count = 0
+race_one_permutations.each_pair do | permutation, result |
+  race_two_count += 1
+  race_two_results[race_two_count] = RacePermutations.new( {RACE_PERMUTATION_INITIALIZATION_MAX_POINTS_KEY => result[ (MAX_NAME + POINTS_NAME) ], RACE_PERMUTATION_INITIALIZATION_LEWIS_POINTS_KEY => result[ (LEWIS_NAME + POINTS_NAME) ]} ).execute_permutations
+end
+
+total_number_of_samples = 0
+max_wins = 0
+lewis_wins = 0
+tie_wins = 0
+race_two_results.each_pair do | permutation, result |
+
+  result.each_pair do | per, res |
+    total_number_of_samples += 1
+    max_wins += 1 if res[ (MAX_NAME + POINTS_NAME) ] > res[ (LEWIS_NAME + POINTS_NAME) ]
+    lewis_wins += 1 if res[ (LEWIS_NAME + POINTS_NAME) ] > res[ (MAX_NAME + POINTS_NAME) ]
+    tie_wins += 1 if res[ (MAX_NAME + POINTS_NAME) ] == res[ (LEWIS_NAME + POINTS_NAME) ]
+    # puts res.inspect if res[ (MAX_NAME + POINTS_NAME) ] == res[ (LEWIS_NAME + POINTS_NAME) ]
+  end
+end
+
+puts "total_number_of_samples: #{total_number_of_samples}"
+puts "Max Wins Championship: #{max_wins}, (#{( (max_wins.to_f / total_number_of_samples) * 100 ).round(5)}%)"
+puts "Lewis Wins Championship: #{lewis_wins}, (#{( (lewis_wins.to_f / total_number_of_samples) * 100 ).round(5)}%)"
+puts "Tied Championship Points: #{tie_wins}, (#{( (tie_wins.to_f / total_number_of_samples) * 100 ).round(5)}%)"
